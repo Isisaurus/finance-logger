900f8a1841b5c991d8aee949262c6414ed220cb9
Isisaurus <dianavitanyi@gmail.com>
2021-10-04 16:32:28 +0200

    Type basics.

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,22 @@
-var character = "mario";
-console.log(character);
 var inputs = document.querySelectorAll("input");
-console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+/*
+-------------------------
+Types in TS
+-------------------------
+    - no floating or int numbers, only Number
+    - can't reassing types, even with let => number can only change into number
+    - inference: infers the type automatically, don't need to assign manually
+    - can assign type to function parameter
+    - TS won't compile if there's an error in type: checks during development
+*/
+var age = 30;
+// age = "none";
+var isBlackBelt = false;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(20));
--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,25 @@ const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
   console.log(input);
 });
+
+/*
+-------------------------
+Types in TS
+-------------------------
+    - no floating or int numbers, only Number
+    - can't reassing types, even with let => number can only change into number
+    - inference: infers the type automatically, don't need to assign manually
+    - can assign type to function parameter
+    - TS won't compile if there's an error in type: checks during development
+*/
+
+let age = 30;
+// age = "none";
+let isBlackBelt = false;
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(20));
