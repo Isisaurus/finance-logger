e66029b7f623815a99f0178415ab18b986554f7c
Isisaurus <dianavitanyi@gmail.com>
2021-10-05 15:39:54 +0200

    Classes in TS.

--- public/app.js
+++ public/app.js
@@ -12,12 +12,30 @@ Notes:
     Type casting:
         - using class selectors TS only knows it is a DOM element, but not the exact type => *type casting* eg.: document.querySelector(".new-item-from") as HTMLFormElement; (! is not needed, it is not null)
 
+    Classes:
+        - can define a class with a constructor with defined parameter types
+        - can define an array type of the class ;)
+        - can edit object properties created from class, but can't change type
+        - can use access modifyers to limit access to these object properties
+
 
 ---------------------------------------------------------------------------------------
 
 */
 // const anchor = document.querySelector("a")!;
 // console.log(anchor.href);
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
 // from
 var form = document.querySelector(".new-item-from");
 // inputs
@@ -30,3 +48,9 @@ form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, toFrom.value, details.value, amount.value);
 });
+var inv1 = new Invoice("mario", "work on the mario website", 50);
+var inv2 = new Invoice("luigi", "work on the luigi website", 300);
+var invoices = [];
+invoices.push(inv1);
+invoices.push(inv2);
+console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,12 @@ Notes:
     Type casting:
         - using class selectors TS only knows it is a DOM element, but not the exact type => *type casting* eg.: document.querySelector(".new-item-from") as HTMLFormElement; (! is not needed, it is not null)
 
+    Classes: 
+        - can define a class with a constructor with defined parameter types
+        - can define an array type of the class ;)
+        - can edit object properties created from class, but can't change type
+        - can use access modifyers to limit access to these object properties
+
 
 ---------------------------------------------------------------------------------------
 
@@ -18,6 +24,23 @@ Notes:
 
 // const anchor = document.querySelector("a")!;
 // console.log(anchor.href);
+
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
 
 // from
 const form = document.querySelector(".new-item-from") as HTMLFormElement;
@@ -33,3 +56,13 @@ form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   console.log(type.value, toFrom.value, details.value, amount.value);
 });
+
+const inv1 = new Invoice("mario", "work on the mario website", 50);
+const inv2 = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(inv1);
+invoices.push(inv2);
+
+console.log(invoices);
