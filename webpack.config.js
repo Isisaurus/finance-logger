21fd0ad8205bece5f3c55f06f94fc7182fdc8215
Isisaurus <dianavitanyi@gmail.com>
2021-10-06 14:09:45 +0200

    Regex rule added to configure webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,16 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/app.ts",
+  module: {
+    rules: [
+      {
+        //   to tell transform ts to js: rule to which file needs to be conferted and bundled uding a regex => needs to end in .ts and where can they found
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
   output: {
     fileName: "bundle.js",
     path: path.resolve(__dirname, "public"),
