63286ba22c9850b715828ff11d8ebbb568a8fe31
Isisaurus <dianavitanyi@gmail.com>
2021-10-06 11:25:39 +0200

    ListTemplate.ts file added: will render the form inputs.

---
+++ src/modules/ListTemplate.ts
@@ -0,0 +1,7 @@
+/*
+    1. register a list  container in the constructor
+    2. create a render mehtod to render a new li to the container
+        -- accepts arguments: invoice or payment, a heading, a position
+        -- create the html template (li, h4, p)
+        -- add the li template to the start/end of the list
+*/
