9699e07879133a3b5ebac5e48e182dea8ca6326b
Isisaurus <dianavitanyi@gmail.com>
2021-10-06 14:05:47 +0200

    Entry relative path and output absolute paths added to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.ts",
+  output: {
+    fileName: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
